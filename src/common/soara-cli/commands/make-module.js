f08f83a57e7f6f42c60e50bdd549ae76054beb6a
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-05-02 14:15:56 +0700

    patch: add gitkeep

--- src/common/soara-cli/commands/make-module.js
+++ src/common/soara-cli/commands/make-module.js
@@ -4,7 +4,7 @@ const { copyTemplate, camelize } = require("@soara/cli/src/utils");
 
 registerCommand("make:module <name>", "Create make:module", async (name) => {
   const templatePath = path.join(__dirname, "../template/module");
-  const fileNamePath = path.join("src/common/modules", camelize(name));
+  const fileNamePath = path.join("src/modules", camelize(name));
   const targetPath = path.join(process.cwd(), fileNamePath);
   copyTemplate(templatePath, targetPath, name);
 });
