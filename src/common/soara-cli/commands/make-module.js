5e1a7645d12745c959f1be3593bd9f8e4a02d4f4
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-05-02 13:49:56 +0700

    patch: add template

--- src/common/soara-cli/commands/make-module.js
+++ src/common/soara-cli/commands/make-module.js
@@ -1,5 +1,10 @@
-const { registerCommand } = require('@soara/cli/src/templates/soara-registry');
+const path = require("path");
+const { registerCommand } = require("@soara/cli/src/templates/soara-registry");
+const { copyTemplate, camelize } = require("@soara/cli/src/utils");
 
-registerCommand('make:module', 'Create make:module', async (name) => {
-  console.log('Hello make:module');
+registerCommand("make:module <name>", "Create make:module", async (name) => {
+  const templatePath = path.join(__dirname, "../template/module");
+  const fileNamePath = path.join("src/common/modules", camelize(name));
+  const targetPath = path.join(process.cwd(), fileNamePath);
+  copyTemplate(templatePath, targetPath, name);
 });
---
+++ src/common/soara-cli/template/module/pages/__Name__.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>Hello __Name__</p>
+</template>
+
+<script setup></script>
+<style lang="scss" scoped></style>
