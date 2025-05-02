cfe94bdee66fa559baa9d8e27ca06244bfdfc8ad
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-05-02 15:48:06 +0700

    patch: update to module

--- src/common/soara-cli/commands/make-module.js
+++ src/common/soara-cli/commands/make-module.js
@@ -1,8 +1,11 @@
-const path = require("path");
-const { registerCommand } = require("@soara/cli/src/templates/soara-registry");
-const { copyTemplate, camelize } = require("@soara/cli/src/utils");
+import path from "path";
+import { fileURLToPath } from "url";
+import { registerCommand } from "@soara/cli/src/templates/soara-registry.js";
+import { copyTemplate, camelize } from "@soara/cli/src/utils/index.js";
 
 registerCommand("make:module <name>", "Create make:module", async (name) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
   const templatePath = path.join(__dirname, "../template/module");
   const fileNamePath = path.join("src/modules", camelize(name));
   const targetPath = path.join(process.cwd(), fileNamePath);
