c3452c3a56d262adde734e163c14141864e67214
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-05-03 10:18:40 +0700

    add load module routes

---
+++ src/common/soara-cli/commands/load-module.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { registerCommand } from '@soara/cli/src/templates/soara-registry.js';
+import { copyTemplate } from '@soara/cli/src/utils/index.js';
+
+registerCommand('load:module <name>', 'Load any data from modules [routes]', async (name) => {
+  if (!name.includes(['routes'])) {
+    console.log(`Unknown to load ${name}`);
+    process.exit();
+  }
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const templatePath = path.join(__dirname, '../template/loaders');
+  const fileNamePath = path.join('src/common/loaders');
+  const targetPath = path.join(process.cwd(), fileNamePath);
+  copyTemplate(templatePath, targetPath, '');
+
+  console.log('Hello load:module');
+});
--- src/common/soara-cli/commands/make-module.js
+++ src/common/soara-cli/commands/make-module.js
@@ -1,13 +1,13 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { registerCommand } from "@soara/cli/src/templates/soara-registry.js";
-import { copyTemplate, camelize } from "@soara/cli/src/utils/index.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { registerCommand } from '@soara/cli/src/templates/soara-registry.js';
+import { copyTemplate, camelize } from '@soara/cli/src/utils/index.js';
 
-registerCommand("make:module <name>", "Create make:module", async (name) => {
+registerCommand('make:module <name>', 'Create new module', async (name) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const templatePath = path.join(__dirname, "../template/module");
-  const fileNamePath = path.join("src/modules", camelize(name));
+  const templatePath = path.join(__dirname, '../template/module');
+  const fileNamePath = path.join('src/modules', camelize(name));
   const targetPath = path.join(process.cwd(), fileNamePath);
   copyTemplate(templatePath, targetPath, name);
 });
